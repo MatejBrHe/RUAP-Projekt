1fbc22e1425bf23cac78ae99807fa5192a3a3bd8
Matej <matejbh@gmail.com>
2024-11-04 15:30:37 +0100

    Kreirana aplikacija i izmjenjena prema uputama.

--- RUAP_test/RUAP_test/RUAP_test.cpp
+++ RUAP_test/RUAP_test/RUAP_test.cpp
@@ -5,7 +5,7 @@
 
 int main()
 {
-    std::cout << "Pozdrav iz aplikacije od Matej Breznicki-Herceg (0165089964).\n";
+    std::cout << "Pozdrav iz aplikacije od Matej Breznicki-Herceg.\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
